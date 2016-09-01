33685deb75ad802e8e06b6444b8a59775e95f95f
Ashwin Balakrishnan <nadarashwin@gmail.com>
2016-09-01 11:34:32 +0530

    modified geocode script with importing keys

--- geocode.py
+++ geocode.py
@@ -2,7 +2,7 @@
 
 import requests
 import json
-
+import key
 
 import sys
 import codecs
@@ -14,9 +14,8 @@ __user = raw_input("please enter the address to be:- ")
 __meal = raw_input("meal type please:- ")
 
 def getGeoCodeLocation(inputstring):
-	google_api_key = ""
 	locationstring = inputstring.replace(" ", "+")
-	url = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" %(locationstring, google_api_key))
+	url = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" %(locationstring, key.google_api_key))
 	data = json.loads(url.text)
 	lat = data['results'][0]['geometry']['location']['lat']
 	lng = data['results'][0]['geometry']['location']['lng']
@@ -25,8 +24,6 @@ def getGeoCodeLocation(inputstring):
 
 def foursquare(inputstring):
 	lt, lg = getGeoCodeLocation(__user)
-	foursquare_client_key = ""
-	foursquare_client_sec = ""
-	url = requests.get("https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20160901&ll=%f,%f&query=%s" %(foursquare_client_key,foursquare_client_sec,lt,lg,inputstring))
+	url = requests.get("https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20160901&ll=%f,%f&query=%s" %(key.foursquare_cli_key,key.foursquare_cli_sec,lt,lg,inputstring))
 	print json.loads(url.text)
 foursquare(__meal)
